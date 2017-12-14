b3bec6b6efa1355a6fbe6bdd53235a1e37ac45d3
jianli20 <31518712+jianli20@users.noreply.github.com>
2017-12-14 13:33:53 -0700

    Made type Interfaces for pokemon

---
+++ src/pokemon/model/Dragon.java
@@ -0,0 +1,8 @@
+package pokemon.model;
+
+public interface Dragon
+{
+	public int dragonClaw(int amount);
+	public int dracoMeteor(int amount);
+	public boolean dragonDance();
+}
---
+++ src/pokemon/model/Ice.java
@@ -0,0 +1,8 @@
+package pokemon.model;
+
+public interface Ice
+{
+	public int iceBeam(int amount);
+	public int blizzard(int amount);
+	public boolean hail();
+}
