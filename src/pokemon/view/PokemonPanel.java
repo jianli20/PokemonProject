6a3f3f56958c6c6c754d934b9e9a5c27388013c2
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-01-02 12:56:18 -0700

    Added GUI datamembers to Panel

--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -1,6 +1,37 @@
 package pokemon.view;
 
-public class PokemonPanel
-{
+import pokemon.controller.PokemonController;
+import javax.swing.*;
 
+public class PokemonPanel extends JPanel
+{
+	private PokemonController appController;
+	private SpringLayout appLayout;
+	
+	private JLabel healthLabel;
+	private JLabel attackLabel;
+	private JLabel nameLabel;
+	private JLabel numberLabel;
+	private JLabel evolvableLabel;
+	private JLabel modifierLabel;
+	private JLabel iconLabel;
+	
+	private JCheckBox evoableBox;
+	private JTextField nameField;
+	private JTextField numberField;
+	private JTextField attackField;
+	private JTextField healthField;
+	private JTextField modifField;
+	
+	private JTextArea descriptivionArea;
+	private JTextArea typeArea;
+	
+	private JButton saveButton;
+	private JButton clearButton;
+	private JComboBox pokedexDropdown;
+	
+	private JPanel firstType;
+	private JPanel secondType;
+	private JPanel thirdType;
+	private JPanel fourthType;
 }
