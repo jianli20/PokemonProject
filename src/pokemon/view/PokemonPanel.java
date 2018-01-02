40bd815a1ddbb6f423c452553a7485228c7ea26f
jianli20 <31518712+jianli20@users.noreply.github.com>
2018-01-02 13:30:21 -0700

    fixed typos, added code to setupPanel method, called setupPanel to constructor

--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -16,14 +16,14 @@ public class PokemonPanel extends JPanel
 	private JLabel modifierLabel;
 	private JLabel iconLabel;
 	
-	private JCheckBox evoableBox;
+	private JCheckBox evolvableBox;
 	private JTextField nameField;
 	private JTextField numberField;
 	private JTextField attackField;
 	private JTextField healthField;
-	private JTextField modifField;
+	private JTextField modifierField;
 	
-	private JTextArea descriptivionArea;
+	private JTextArea descriptionArea;
 	private JTextArea typeArea;
 	
 	private JButton saveButton;
@@ -34,4 +34,78 @@ public class PokemonPanel extends JPanel
 	private JPanel secondType;
 	private JPanel thirdType;
 	private JPanel fourthType;
+	
+	private void updatePokedexInfo(int index)
+	{
+		nameField.setText(appController.getPokedex().get(index).getName());
+		evolvableBox.setSelected(appController.getPokedex().get(index).isCanEvolve());
+		numberField.setText(appController.getPokedex().get(index).getNumber() + "");
+		attackField.setText(appController.getPokedex().get(index).getAttackPoints() + "");
+		healthField.setText(appController.getPokedex().get(index).getHealthPoints() + "");
+		modifierField.setText(appController.getPokedex().get(index).getEnhancementModifier() + "");
+	}
+	
+	public PokemonPanel(PokemonController appController)
+	{
+		super();
+		this.appController = appController;
+		setupPanel();
+		
+		healthLabel = new JLabel("Health");
+		attackLabel = new JLabel("Attack");
+		nameLabel = new JLabel("Name");
+		numberLabel = new JLabel("Pokedex Number");
+		evolvableLabel = new JLabel("Evolvable");
+		modifierLabel = new JLabel("Modifier");
+		iconLabel = new JLabel ();
+		
+		evolvableBox = new JCheckBox();
+		nameField = new JTextField();
+		numberField = new JTextField();
+		attackField = new JTextField();
+		healthField = new JTextField();
+		modifierLabel = new JLabel();
+		
+		descriptionArea = new JTextArea();
+		typeArea = new JTextArea();
+		
+		saveButton = new JButton();
+		clearButton = new JButton();
+		pokedexDropdown = new JComboBox();
+		
+		firstType = new JPanel();
+		secondType = new JPanel();
+		thirdType = new JPanel();
+		fourthType = new JPanel();
+	}	
+		public void setupPanel()
+		{
+		this.add(healthLabel);
+		this.add(attackLabel);
+		this.add(nameLabel);
+		this.add(numberLabel);
+		this.add(evolvableLabel);
+		this.add(modifierField);
+		this.add(iconLabel);
+		
+		this.add(evolvableBox);
+		this.add(nameField);
+		this.add(numberField);
+		this.add(attackField);
+		this.add(healthField);
+		this.add(modifierLabel);
+		
+		this.add(descriptionArea);
+		this.add(typeArea);
+		
+		this.add(saveButton);
+		this.add(clearButton);
+		this.add(pokedexDropdown);
+		
+		this.add(firstType);
+		this.add(secondType);
+		this.add(thirdType);
+		this.add(fourthType);
+		}
+	
 }
